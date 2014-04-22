01a6823e6aff4e209d5116c13e7466cd37e0444a
Sean Stephenson <sms@pixar.com>
2014-04-21 22:10:26 -0700

    return st and not json

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return {"message": "Hello World!"}
+    return 'hello world'
+    #return {"message": "Hello World!"}
 
 if __name__ == "__main__":
     app.run()
